3fd55c6db923095922d5f19b85d817c506d37651
Metaphox <foxisme@gmail.com>
2026-04-01 01:22:00 +0200

    viper and cobra. maybe I don't really need cobra

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -1,26 +1,36 @@
 /*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2026 Tao Wu <foxisme@gmail.com>
 */
+
 package cmd
 
 import (
 	"os"
+	"errors"
+	"strings"
+	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 
 
 // rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "underdash",
-	Short: "_ ask LLM for help one line at a time",
-	Long: `Underdash is a non interactive coding agent.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
-}
+var (
+	cfgFile string
+	rootCmd = &cobra.Command{
+		Use:   "underdash",
+		Short: "_ ask LLM for help one line at a time",
+		Long: `Underdash is a non interactive coding agent.`,
+		// Uncomment the following line if your bare application
+		// has an action associated with it:
+		Run: func(cmd *cobra.Command, args []string) { },
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			return initializeConfig(cmd)
+		},
+	}
+)
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
@@ -32,15 +42,38 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.underdash.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default location is $HOME/.config/underdash/config.yaml)")
 }
 
+func initializeConfig(cmd *cobra.Command) error {
+	viper.SetEnvPrefix("UNDERDASH")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "*", "-", "*"))
+	viper.AutomaticEnv()
 
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		viper.AddConfigPath(home + "/.config/underdash")
+		viper.SetConfigName("config")
+		viper.SetConfigType("yaml")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		// config file missing
+		var configFileNotFoundError viper.ConfigFileNotFoundError
+		if !errors.As(err, &configFileNotFoundError) {
+			return err
+		}
+	}
+
+	err := viper.BindPFlags(cmd.Flags())
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Configuration initialized. Using config file:", viper.ConfigFileUsed())
+	return nil
+}
--- src/main.go
+++ src/main.go
@@ -4,7 +4,7 @@ Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "underdash/cmd"
+import "metaphox/underdash/cmd"
 
 func main() {
 	cmd.Execute()
